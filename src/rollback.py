89da4ae20ff884d9e9c0103bb7ac43b432c9dfa3
Stefanos I. Tsaklidis <stefanos@tsaklidis.gr>
2024-01-21 03:10:58 +0200

    Add completed ifo

--- src/rollback.py
+++ src/rollback.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     account = Actions('old')
     database = DB()
     to_rollback = database.get_pending_rollbacks()
+    success_rolls = 0
 
     log(f'Found {len(to_rollback)} pending rollbacks')
     log(f'Try to send them back online (Y/N)?')
@@ -33,9 +34,12 @@ if __name__ == "__main__":
 
                 # Rollback sent back online, no reason to keep it in DB. Delete
                 database.delete_record(item_id)
+                success_rolls += 1
             except Exception as e:
                 log('Could not rollback, Comment/Submission not found',
                     'error')
+        log(f'Rollback finished! Completed {success_rolls}/{len(to_rollback)}')
+        quit('Bye!')
     else:
         log('Rollback cancelled.')
         quit('Bye!')
